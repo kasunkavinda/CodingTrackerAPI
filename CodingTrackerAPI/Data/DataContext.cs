2b3ed6fdd5a5377bb565fffa30322577d3938c86
Kasun <kasunkavinda.bc@gmail.com>
2022-01-30 08:06:29 +0530

    Add project files.

---
+++ CodingTrackerAPI/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodingTrackerAPI.Data
+{
+    public class DataContext: DbContext
+    {
+
+    }
+}
---
+++ CodingTrackerAPI/OverviewCodingData.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingTrackerAPI
+{
+    public class OverviewCodingData
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = string.Empty;
+
+        [StringLength(200)]
+        public string Cooments { get; set; } = string.Empty;
+
+        public int CodingTypeId { get; set; }
+
+        public CodingType? CodingType { get; set; }
+    }
+}
